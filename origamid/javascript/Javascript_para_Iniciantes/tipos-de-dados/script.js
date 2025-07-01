de73020325b92ff3b1d3882ac3252f817171c52b
Tonho <antonioreverso.com@gmail.com>
2025-07-01 14:47:46 -0300

    remove unused HTML and JavaScript files; add .gitignore for Go and Kotlin projects

--- origamid/javascript/Javascript_para_Iniciantes/tipos-de-dados/script.js
+++
@@ -1,7 +0,0 @@
-var name = 'Tonho'
-var number = '56'
-var age = 243
-var last_name = 'Galhaço'
-var final_name = name + last_name
-var phrase = "It's time"
-typeof (name)
--- origamid/javascript/Javascript_para_Iniciantes/variaveis/script.js
+++
@@ -1,9 +0,0 @@
-var nome = "tonho";
-var idade = 171;
-var comidaFavorita;
-pastel = "pastel";
-var um,
-    dois,
-    tres,
-    quatro,
-    cinco;
