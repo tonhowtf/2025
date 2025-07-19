1b86ba5c32948c1089a98bc78de1c143a10ba87d
Tonho <antonioreverso.com@gmail.com>
2025-07-18 22:52:23 -0300

    feat: Func build

---
+++ Rafael_Camarda/Elixir_e_Phoenix/fizzbuzz/ex_mon/lib/player.ex
@@ -0,0 +1,15 @@
+defmodule ExMon.Player do
+  required_keys = [:life, :name, :move_rnd, :move_avg, :move_heal]
+  @enforce_keys required_keys
+  defstruct required_keys
+
+  def build(name, move_rnd, move_avg, move_heal) do
+    %ExMon.Player{
+      name: name,
+      move_rnd: move_rnd,
+      move_avg: move_avg,
+      move_heal: move_heal,
+      life: 100
+    }
+  end
+end
