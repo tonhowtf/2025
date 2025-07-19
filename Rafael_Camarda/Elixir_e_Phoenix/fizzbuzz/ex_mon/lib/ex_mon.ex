db6f941c5c3da45b4a3b9dc86ee01b63ecc82d55
Tonho <antonioreverso.com@gmail.com>
2025-07-19 01:18:21 -0300

    refactor: computer as constant

--- Rafael_Camarda/Elixir_e_Phoenix/fizzbuzz/ex_mon/lib/ex_mon.ex
+++ Rafael_Camarda/Elixir_e_Phoenix/fizzbuzz/ex_mon/lib/ex_mon.ex
@@ -1,12 +1,14 @@
 defmodule ExMon do
   alias ExMon.{Game, Player}
 
+  @computer_name "marvin"
+
   def create_player(name, move_avg, move_rnd, move_heal) do
     Player.build(name, move_rnd, move_avg, move_heal)
   end
 
   def start_game(player) do
-    "marvin"
+    @computer_name
     |> create_player(:punch, :kick, :heal)
     |> Game.start(player)
   end
