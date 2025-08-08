a5cc4309a667943be3b9aa2c8bd02fbc9ab64f3d
Tonho <antonioreverso.com@gmail.com>
2025-08-08 19:32:15 -0300

    feat: implement game move logic and enhance status messages

--- Rafael_Camarda/Elixir_e_Phoenix/fizzbuzz/ex_mon/lib/game/status.ex
+++ Rafael_Camarda/Elixir_e_Phoenix/fizzbuzz/ex_mon/lib/game/status.ex
@@ -2,8 +2,12 @@ defmodule ExMon.Game.Status do
   alias ExMon.Game
 
   def print_round_message() do
-    IO.puts("The game is started! ===\n")
+    IO.puts("===\n The game is started! ===\n")
     IO.inspect(Game.info())
     IO.puts("---------------------")
+  end
+
+  def print_wrong_move_message(move) do
+    IO.puts("\n=== Invalid move: #{move} ===\n")
   end
 end
